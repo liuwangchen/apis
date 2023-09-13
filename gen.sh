7f2325ef8f3bdc97b3eff169d882fe2eaf6bde14
riven <riven@jiemogame.com>
2023-09-13 19:42:42 +0800

    [update] init apis

--- gen.sh
+++ gen.sh
@@ -4,11 +4,18 @@ set -ex
 CURDIR=$(cd $(dirname $0); pwd)
 export PATH=$PATH:.
 
+# go
+protoc --proto_path=$CURDIR/ \
+  --go_out=$CURDIR/apipb --go_opt=paths=source_relative \
+  $CURDIR/ret.proto \
+
+
 # natsrpc and httprpc
 protoc --proto_path=$CURDIR/rpc \
   -I=$CURDIR \
   --go_out=$CURDIR/rpc --go_opt=paths=source_relative \
   --natsrpc_out=$CURDIR/rpc --natsrpc_opt=paths=source_relative \
+  --http_out=$CURDIR/rpc --http_opt=paths=source_relative \
   $CURDIR/rpc/*/*.proto \
 
-mv $CURDIR/rpc/*/*.go ./pb && rm -f $CURDIR/rpc/*/*.go
+mv $CURDIR/rpc/*/*.go ./apipb && rm -f $CURDIR/rpc/*/*.go
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.5.3
-	github.com/liuwangchen/toy v0.0.0-20230913062353-2262af006c4c
+	github.com/liuwangchen/toy v0.0.0-20230913063024-b6a201ebcc9e
 	google.golang.org/protobuf v1.31.0
 )
